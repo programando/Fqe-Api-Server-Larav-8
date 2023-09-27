e71c6730e4ccafaa21c1a8280f356a875108b7bb
programando <jhonjamesmg@hotmail.com>
2023-09-27 08:47:55 -0500

    cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-             'cors',
+            'cors',
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
