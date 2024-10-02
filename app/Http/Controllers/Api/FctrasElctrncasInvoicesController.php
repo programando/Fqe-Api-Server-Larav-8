4383ef1a69bd138292ba5c85e0a57ed6b11109f7
programando <jhonjamesmg@hotmail.com>
2024-10-02 15:53:54 -0500

    docs

--- app/Http/Controllers/Api/FctrasElctrncasInvoicesController.php
+++ app/Http/Controllers/Api/FctrasElctrncasInvoicesController.php
@@ -209,13 +209,13 @@ class FctrasElctrncasInvoicesController
              
             foreach ($Documentos as $Documento ) {
                 $this->invoicesToSend ( $Documento) ;   
-                // if  ( $Documento->is_export == false) {
-                //     $response   = $this->ApiSoenac->postRequest( $URL, $this->jsonObject ) ;    
-                // }else {
-                //     $response   = $this->ApiSoenac->postRequest( 'export-invoice', $this->jsonObject ) ;  
-                // }
-                //$this->traitUpdateJsonObject ( $Documento );
-                //$this->documentsProcessReponse( $Documento, $response ) ;
+                if  ( $Documento->is_export == false) {
+                    $response   = $this->ApiSoenac->postRequest( $URL, $this->jsonObject ) ;    
+                }else {
+                    $response   = $this->ApiSoenac->postRequest( 'export-invoice', $this->jsonObject ) ;  
+                }
+                $this->traitUpdateJsonObject ( $Documento );
+                $this->documentsProcessReponse( $Documento, $response ) ;
                  return $this->jsonObject;
             }  
         }
