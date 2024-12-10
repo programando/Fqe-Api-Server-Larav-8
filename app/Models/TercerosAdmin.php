d98370cddbffdfa5b8db80ee568f4ec11eab638e
programando <jhonjamesmg@hotmail.com>
2024-12-10 11:30:53 -0500

    Administradores

--- app/Models/TercerosAdmin.php
+++ app/Models/TercerosAdmin.php
@@ -8,15 +8,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-/**
- * Class TercerosAdmin
- * 
- * @property int $idregistro
- * @property string $email
- * @property string $number_code
- *
- * @package App\Models
- */
+ 
 class TercerosAdmin extends Model
 {
 	protected $table = 'terceros_admins';
