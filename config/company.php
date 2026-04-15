6bd3f2a34e75d875ca130e433736942cb178841f
programando <jhonjamesmg@hotmail.com>
2026-04-15 18:17:08 -0500

    feat: add company configuration file for environment variables and business constants

--- config/company.php
+++ config/company.php
@@ -6,7 +6,7 @@
       'NOMINA_ELECT_URL_BASE'  => env('NOMINA_ELECT_URL_BASE'),
 
       'EMPRESA'                     => 'F.Q.E. S.A.S.',
-      'NIT'                         => ' 901615644',
+      'NIT'                         => '901615644',
       'DIRECCION'                   => 'CALLE 35 # 4 - 31',
       'APP_URL'                     => env('APP_URL'),
       'APP_URL_CLIENT'              => env('APP_URL_CLIENT'),
